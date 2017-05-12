82fed1a9221f5a24ce2ab8ec89dc7847f8feaaa9
JoeMalone <forsikringsmanden@gmail.com>
2017-05-12 23:55:18 +0200

    facebook login

---
+++ Butlerstar/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  Butlerstar
+//
+//  Created by Jonas Elholm on 12/05/2017.
+//  Copyright © 2017 ButlerStar. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
